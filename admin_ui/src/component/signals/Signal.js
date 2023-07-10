952ebfaa6b055695348e0daa8a15cb4410d4571c
DragonFlameStrike <d.pankov@g.nsu.ru>
2023-07-10 19:35:19 +0700

    master/FRONT-001 : Добавил библиотеку для построения графиков и организовал пространство для будущей работы

--- admin_ui/src/component/signals/Signal.js
+++ admin_ui/src/component/signals/Signal.js
@@ -1,16 +1,67 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+
 
 class Signal extends React.Component {
+    options;
+    data;
     constructor(props) {
         super(props);
     }
 
     render() {
+        console.log(this.props.signal)
+        this.options = {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: this.props.name,
+                },
+            },
+        };
+        this.data = {
+            labels,
+            datasets: [
+                {
+                    label: 'Dataset 1',
+                    data: labels.map(() => 1),
+                    borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                },
+            ],
+        };
         return(
-            <div>
-                <p>Сигнал</p>
-            </div>
+            <Line options={this.options} data={this.data} />
         )
     }
 }
--- admin_ui/src/component/signals/Signals.js
+++ admin_ui/src/component/signals/Signals.js
@@ -4,29 +4,39 @@ import Signal from "./Signal";
 
 
 const Signals = (props) => {
-    const [signals, setSignals] = useState([]);
+    const [tsunamiSignal, setTsunamiSignal] = useState([]);
+    const [mainSignal, setMainSignal] = useState([]);
+    const [extrapolatedSignal, setExtrapolatedSignal] = useState([]);
+    const [filteredSignal, setFilteredSignal] = useState([]);
 
     useEffect(() => {
         axios.get('http://localhost:8080/api/signal/filter')
             .then(response => {
-                setSignals(response.data);
+                setMainSignal(response.data.mainSignal[0]);
+                setTsunamiSignal(response.data.tsunamiSignal[0]);
+                setExtrapolatedSignal(response.data.extrapolatedSignal[0]);
+                setFilteredSignal(response.data.filteredSignal[0]);
             })
             .catch(error => {
                 console.log(error);
             });
     }, []);
 
-
-    const sections = Array.from({ length: 4}).map((_, index) => (
-        <div  key={index}>
-            <Signal/>
-        </div>
-    ));
-
     return(
-        <section className="content">
-            {sections}
-        </section>
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '100px' , justifyContent: 'center', alignItems: 'center'}}>
+            <div style={{ flexBasis: 'calc(40% - 20px)' }}>
+                <Signal signal={mainSignal} name = {"Входной сигнал"}/>
+            </div>
+            <div style={{ flexBasis: 'calc(40% - 20px)' }}>
+                <Signal signal={extrapolatedSignal} name = {"Подготовленный к фильрации сигнал"}/>
+            </div>
+            <div style={{ flexBasis: 'calc(40% - 20px)' }}>
+                <Signal signal={filteredSignal} name = {"Отфильтрованный сигнал"}/>
+            </div>
+            <div style={{ flexBasis: 'calc(40% - 20px)' }}>
+                <Signal signal={tsunamiSignal} name = {"Цунами компонента"}/>
+            </div>
+        </div>
     );
 };
 
